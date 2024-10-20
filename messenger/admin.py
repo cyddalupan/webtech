ca48e4aa3e3a134708f3d160f8fe7d362db5bbae
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-10-20 20:28:19 +0800

    hook 3

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile, Chat
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('facebook_id', 'full_name', 'age', 'location')  # Customize what you want to see
+    search_fields = ('facebook_id', 'full_name')  # Add search capability by these fields
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'reply', 'timestamp')  # Customize what is shown in the admin list view
+    list_filter = ('timestamp',)  # Filter chats by date
+    search_fields = ('user__full_name', 'message', 'reply')  # Add search capabilities on user, message, and reply
