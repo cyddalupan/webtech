be6858af837dd6e3c14f8737bbec89b7e414daa3
cyd dalupan <cydmdalupan@gmail.com>
2024-10-31 04:48:53 +0000

    setup admin and model

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FacebookPage
 
-# Register your models here.
+@admin.register(FacebookPage)
+class FacebookPageAdmin(admin.ModelAdmin):
+    list_display = ('page_id', 'name', 'details')
+    search_fields = ('name', 'page_id')
+    list_filter = ('name',)
--- page/models.py
+++ page/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class FacebookPage(models.Model):
+    page_id = models.CharField(max_length=50, unique=True) 
+    name = models.CharField(max_length=255)
+    details = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Facebook Page"
+        verbose_name_plural = "Facebook Pages"
